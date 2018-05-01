e14d163466cf0b2df9dfb029c63002aa22b8b173
Tuturu-chan <peteranh3105@gmail.com>
2018-05-01 15:20:56 +0800

    "Moved lighting calculation to fragment shader (Task H)"

--- project/LINUX_VERSIONS/fStart.glsl
+++ project/LINUX_VERSIONS/fStart.glsl
@@ -1,10 +1,45 @@
-varying vec4 color;
+//varying vec4 color;
 varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
 
 uniform sampler2D texture;
 uniform float texScale; //Part B: texture scaling
 
+varying vec3 fN; 
+varying vec3 fE; 
+varying vec3 fL;
+
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct; 
+uniform mat4 ModelView;
+uniform mat4 Projection;
+uniform vec4 LightPosition;
+uniform float Shininess;
+
+
+
 void main()
 {
-    gl_FragColor = color * texture2D( texture , texCoord * 2.0 * texScale);
+    //From lecture 17 (task H)
+    vec3 N = normalize(fN);
+    vec3 E = normalize(fE);
+    vec3 L = normalize(fL);
+
+    vec3 H = normalize(L+E); //half-way vector
+
+    vec3 ambient = AmbientProduct;
+
+    float Kd = max(dot(L,N), 0.0);
+    vec3 diffuse = Kd * DiffuseProduct;
+
+    float Ks = pow(max(dot(N,H),0.0), Shininess);
+    vec3 specular = Ks * SpecularProduct;
+
+    if(dot(L,N) < 0.0)
+    {
+        specular = vec3(0.0,0.0,0.0);
+    }
+
+    // globalAmbient is independent of distance from the light source
+    vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
+
+    gl_FragColor = vec4(globalAmbient + ambient + diffuse + specular, 1.0) * texture2D( texture , texCoord * 2.0 * texScale);
 }
--- project/LINUX_VERSIONS/vStart.glsl
+++ project/LINUX_VERSIONS/vStart.glsl
@@ -1,27 +1,36 @@
-attribute vec3 vPosition;
+attribute vec4 vPosition; //change vPosition type to vec4
 attribute vec3 vNormal;
 attribute vec2 vTexCoord;
 
 varying vec2 texCoord;
-varying vec4 color;
+//varying vec4 color;
+//No need this line as the lighting will be calculated in fragment shader
 
-uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+//Add these following variables which will be passed down to fragment shader for per-frag lighting calculation
+varying vec3 fN;
+varying vec3 fL;
+varying vec3 fE;
+
+//uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct; <-- move to fragment shader
 uniform mat4 ModelView;
 uniform mat4 Projection;
 uniform vec4 LightPosition;
-uniform float Shininess;
+//uniform float Shininess; <-- move to fragment shader
 
 void main()
 {
-    vec4 vpos = vec4(vPosition, 1.0);
+
+    //vec4 vpos = vec4(vPosition, 1.0); --> vPosition is vec4 now;
 
     // Transform vertex position into eye coordinates
-    vec3 pos = (ModelView * vpos).xyz;
+    vec3 pos = (ModelView * vPosition).xyz;
 
 
     // The vector to the light from the vertex    
-    vec3 Lvec = LightPosition.xyz - pos;
-
+    fL = LightPosition.xyz - pos;            // Direction to the light source
+    fE = -pos;                               // Direction to the eye/camera
+    fN = (ModelView*vec4(vNormal, 0.0)).xyz; // Transform vertex normal into eye coordinates
+    /*
     // Unit direction vectors for Blinn-Phong shading calculation
     vec3 L = normalize( Lvec );   // Direction to the light source
     vec3 E = normalize( -pos );   // Direction to the eye/camera
@@ -48,7 +57,9 @@ void main()
     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
     color.rgb = globalAmbient  + ambient + diffuse + specular;
     color.a = 1.0;
+    */
+    //Everything above is moved to fragment shader (task G)
 
-    gl_Position = Projection * ModelView * vpos;
+    gl_Position = Projection * ModelView * vPosition;
     texCoord = vTexCoord;
 }
